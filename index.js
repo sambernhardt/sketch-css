3f4902499107749968942540d08d38714efacd32
Samuel Bernhardt <sdbernhardt@gmail.com>
2019-02-24 13:49:02 -0500

    Pre repo push

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var chokidar = require('chokidar');
 var sketchCSSCompile = require('./scripts/compile');
 
 // compile on initial run
-sketchCSSCompile.compile();
+sketchCSSCompile.compile('./design.sketch', './site/styles.css');
 
 // watch for changes to the sketch file and run the compiler
 chokidar.watch('./design.sketch').on("change", () => {
-  sketchCSSCompile.compile();
+  sketchCSSCompile.compile('./design.sketch', './site/styles.css');
 })
--- scripts/compile.js
+++ scripts/compile.js
@@ -7,35 +7,31 @@ var sass = require('sass');
 var compileSymbolStyles = require('./symbolStyles');
 var compileTextStyles = require('./textStyles');
 
-function compile() {
-  ns.read('./design.sketch').then(sketch => {
-    var styles = [];
+module.exports = {
+  compile: function(sketchFile, dest) {
+    ns.read(sketchFile).then(sketch => {
+      var styles = [];
 
-    // Compile Text Styles
-    compileTextStyles.compile(sketch, function(textStyles) {
-      styles = styles.concat(textStyles);
+      // Compile Text Styles
+      compileTextStyles.compile(sketch, function(textStyles) {
+        styles = styles.concat(textStyles);
 
-      // Compile Symbol Styles
-      compileSymbolStyles.compile(sketch, function(layerStyles) {
-        styles = styles.concat(layerStyles);
+        // Compile Symbol Styles
+        compileSymbolStyles.compile(sketch, function(layerStyles) {
+          styles = styles.concat(layerStyles);
+        })
+
+        // Combine array of lines into string of text
+        var combinedStyles = styles.join('\n');
+        // Beautify
+        styles = beautify(combinedStyles, {indent: "  "});
       })
 
-      // Combine array of lines into string of text
-      var combinedStyles = styles.join('\n');
-      // Beautify
-      styles = beautify(combinedStyles, {indent: "  "});
+      // Write CSS to file
+      fs.writeFile(dest, styles, (err) => {
+        if (err) console.log(err);
+        console.log("CSS written.");
+      });
     })
-
-    // Write CSS to file
-    fs.writeFile('./site/styles.css', styles, (err) => {
-      if (err) console.log(err);
-      console.log("CSS written.");
-    });
-  })
-}
-
-module.exports = {
-  compile: function() {
-    compile()
   }
 }
